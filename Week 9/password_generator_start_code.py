bfc702ca965fdc958fa91804c07b3e5a585c6083
teachops <104358175+teachops@users.noreply.github.com>
2022-04-26 08:27:11 +0000

    Starter Pack for Week 9

---
+++ Week 9/password_generator_start_code.py
@@ -0,0 +1,80 @@
+"""
+    Password Generator Project
+    input:  password length
+            Number of Digits
+            Number of Symbols
+    
+    output: password (as string)
+"""
+####################################################################################################
+######################## DO NOT CHANGE THIS SECTION OF THE CODE ####################################
+# importing the relevant modules
+import string
+import random
+
+# List of all lowercase and uppercase characters in Python
+all_alphabets = list(string.ascii_letters)
+
+all_digits = list(string.digits)
+
+all_symbols = list(string.punctuation)
+
+#########################################################################################################
+
+
+
+####################################### TYPE YOU CODE IN HERE! ##########################################
+# Ask user for their how many letters they would like in their password and cast to integer and store as no_of_letters
+
+
+# Ask user for how many symbols they would like in their password and cast to integer and store as no_of_symbols
+
+
+# Ask user for how many digits they would like in their password and cast to integer and store as no_of_digits
+
+
+# Set Accumulator for Password Characters List
+
+
+# Get the random letter for the password
+# Randomly Select the characters
+# for alphabet in range(no_of_letters):
+#       select a random characer from list of alphabets and append to the password characters list 
+
+
+
+# Get the Random Symbols for the password symbols
+
+# Set Accumulator for the Number of Symbols List
+
+
+# Randomly Select the characters
+# for alphabet in range(no_of_symbols):
+#       select a random symbol from list of symbols and append to the password symbols list 
+
+
+
+
+# Get the Random Digits for the password
+
+# Set Accumulator for the Number of Digits List
+
+# Randomly Select the characters
+# for alphabet in range(no_of_digits):
+#       select a random digit from list of digits and append to the password digits list 
+
+
+# Add the lists to get the final password and shuffle the final_password_list
+
+
+# To shuffle, items in a list https://www.w3schools.com/python/ref_random_shuffle.asp 
+
+# Now that we have our password in a list lets change that to a string then print it
+
+# accumulator for string_password
+
+
+# for character in final_password_list:
+#       append the character to the string_password
+
+# Print the string password
